71d9e5b531366aff46854b864063c211ad4bbc3c
Nathan <NathanBox@protonmail.com>
2021-04-27 09:53:47 +1000

    Rotating star backdrop

---
+++ LiminalBlankProject/Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 rotation;
+
+    void Update() {
+        transform.localRotation *= Quaternion.Euler(rotation * Time.deltaTime);
+    }
+}
