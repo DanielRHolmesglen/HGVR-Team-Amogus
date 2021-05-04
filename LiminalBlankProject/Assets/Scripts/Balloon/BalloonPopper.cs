2cf2303f60b56f6729f2355fcb4b1717a97627b5
Nathan <NathanBox@protonmail.com>
2021-05-04 14:25:30 +1000

    Add balloon that can pop
    Add added cleaner collision meshes for hot air balloon parts

---
+++ LiminalBlankProject/Assets/Scripts/Balloon/BalloonPopper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BalloonPopper : MonoBehaviour
+{
+    [SerializeField]
+    MeshRenderer normalMesh;
+    [SerializeField]
+    SkinnedMeshRenderer fragmentedMesh;
+    [SerializeField]
+    Transform fragmentedTransform;
+
+    [SerializeField]
+    AudioClip[] popSoundPool;
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    bool randomizeRotation = true;
+    [SerializeField]
+    float lifetimeAfterPop = 0.5f;
+    [SerializeField]
+    UnityEvent popEnded;
+
+    float popping = 0.0f;
+    public void Pop()
+    {
+        popping = 0f;
+        this.enabled = true;
+        fragmentedMesh.enabled = true;
+        normalMesh.enabled = false;
+        if (randomizeRotation)
+            fragmentedTransform.rotation = Quaternion.AngleAxis(Random.value * 360f, Vector3.up);
+
+        if (popSoundPool.Length != 0)
+        {
+            audioSource.pitch = Random.Range(1.1f, 1.3f);
+            audioSource.PlayOneShot(popSoundPool[Random.Range(0, popSoundPool.Length)]);
+        }
+    }
+
+    void Awake()
+    {
+        this.enabled = false;
+        fragmentedMesh.enabled = false;
+    }
+
+    void Update()
+    {
+        if (popping == 1.0f + lifetimeAfterPop)
+        {
+            popping = 0f;
+            fragmentedTransform.localScale = Vector3.zero;
+            fragmentedMesh.SetBlendShapeWeight(0, 0f);
+            fragmentedMesh.enabled = false;
+            this.enabled = false;
+            normalMesh.enabled = true;
+            popEnded?.Invoke();
+            return;
+        }
+        popping = Mathf.Min(1.0f + lifetimeAfterPop, popping + Time.deltaTime * 3.5f);
+
+        float x = Mathf.Min(1.0f, popping);
+        float v = 2.70158f * x * x * x - 1.70158f * x * x;
+        fragmentedTransform.localScale = Vector3.one * (1f - v);
+        fragmentedMesh.SetBlendShapeWeight(0, Mathf.Sin(x * .5f * Mathf.PI) * 100f);
+    }
+}
---
+++ LiminalBlankProject/Assets/Scripts/Balloon/Editor/BalloonEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BalloonPopper))]
+public class BalloonPopperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (Application.isPlaying && GUILayout.Button("Pop"))
+            ((BalloonPopper)target).Pop();
+
+        base.OnInspectorGUI();
+    }
+}
