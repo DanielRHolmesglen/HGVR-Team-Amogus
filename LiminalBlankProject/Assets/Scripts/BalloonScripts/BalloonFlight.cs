ccda55efa3029e7b477a0d33d1aa25bdebebb564
JR0z3 <80295252+JR0z3@users.noreply.github.com>
2021-05-10 10:46:50 +1000

    Created balloon spawning script and separate test scene for them, also a countdown timer script.

---
+++ LiminalBlankProject/Assets/Scripts/BalloonScripts/BalloonFlight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This script controls the flight of the balloons in the game, it can also work for the island movement.
+/// </summary>
+public class BalloonFlight : MonoBehaviour
+{
+    public float speed = 1;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+    }
+}
+
---
+++ LiminalBlankProject/Assets/Scripts/BalloonScripts/BalloonWaves.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using UnityEngine;
+/// <summary>
+/// WaveSystem controls balloons spawning
+/// </summary>
+
+public class BalloonWaves : MonoBehaviour
+{
+    //Setting the different states of spawning at spawn points.
+    public enum SpawnState { SPAWNING, WAITING, COUNTING }
+
+    //Wave holds important variables.
+    [System.Serializable]
+    public class Wave
+    {
+        //Name of the wave.
+        public string name;
+        //Referene to our enemy gameobject.
+        public Transform enemy;
+        //Amount of the enemies.
+        public int enemyAmount;
+        //Rate which the enemies spawn into the map.
+        public float spawnRate;
+    }
+    //An array to make separate waves.
+    public Wave[] waves;
+    //Stores the index of the next wave.
+    private int nextWave = 0;
+
+    //Gets the next wave.
+    public int NextWave
+    {
+        get { return nextWave + 1; }
+    }
+    //Set objects as spawnpoints.
+    public Transform[] spawnPoints;
+    //The time that the player will have between waves to prepare and collect collectibles.
+    public float timeBewteenWaves = 5f;
+    //Countdown down between the waves.
+    private float waveCountdown;
+    //Time for 
+    private float searchCountdown = 1f;
+    //Giving counting spawn state a variable
+    private SpawnState state = SpawnState.COUNTING;
+    //
+    void Start()
+    {
+        waveCountdown = timeBewteenWaves;
+    }
+
+    void Update()
+    {
+        //If the player is waiting then check if an enemy is alive.
+        if (state == SpawnState.WAITING)
+        {
+            //If our enemy isn't alive then our wave is completed.
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        //If the countdown for the wave has reached 0, start spawning the next wave.
+        if (waveCountdown <= 0)
+        {
+            //If the game is no longer in the counting stage, spawn enemies.
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        //Otherwise keep counting down.
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    //Moves to the next wave.
+    void WaveCompleted()
+    {
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBewteenWaves;
+
+        //Counts down the waves.
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    //Checking if an enemy is alive.
+    bool EnemyIsAlive()
+    {
+
+        searchCountdown -= Time.deltaTime;
+        //If our countdown reaches 0, wait a second spawn enemies.
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            //If there are no objects with the tag enemy then 
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Spawn enemy and wait.
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.enemyAmount; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.spawnRate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    //When called spawns the enemy at assigned spawnpoints.
+    void SpawnEnemy(Transform _enemy)
+    {
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+    }
+}
